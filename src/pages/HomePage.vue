6597f9dc34836bf703db9ff540685d19df304a1c
Isidora Vujasin <isidoravujasin@Isidoras-MacBook-Air.local>
2026-01-20 10:11:25 +0100

    feat: add basic pages skeleton

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup>
+  const title = 'Vue Basics';
+</script>
+
+<template>
+  <main>
+  <h1>{{title}}</h1>
+  <p> Mini blog using JSONPlaceholder. </p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+  font-family: system-ui, sans-serif;
+}
+</style>
---
+++ src/pages/PostsPage.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <main>
+  <h1>Posts</h1>
+  <p> List of posts will go here. </p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+  font-family: system-ui, sans-serif;
+}
+</style>
