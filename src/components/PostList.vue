2e006f5e0313d9336fdd07ea28a126cdcefb029d
Isidora Vujasin <isidoravujasin@Isidoras-MacBook-Air.local>
2026-01-20 11:57:42 +0100

    feat: add PostList component

---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { RouterLink } from 'vue-router'
+
+    defineProps({
+        posts: {
+            type: Array,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <ul class="list">
+        <li v-for="post in posts" :key="post.id" class="item">
+            <RouterLink :to="`/posts/${post.id}`" class="link">
+                {{ post.title }}
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.list { padding-left: 1.2rem; }
+.item { margin: 0.35rem 0; }
+.link { text-decoration: none; }
+.link:hover { text-decoration: underline; }
+</style>
