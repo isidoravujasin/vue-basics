eb26b59f8739b032656c8772112282589023b845
Isidora Vujasin <isidoravujasin@Isidoras-MacBook-Air.local>
2026-01-20 11:49:26 +0100

    feat: add route for post details

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomePage from '../pages/HomePage.vue';
+import HomePage from '@/pages/HomePage.vue';
 import PostsPage from '@/pages/PostsPage.vue';
+import PostDetailPage from '@/pages/PostDetailPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomePage },
-    { path: '/posts', name: 'posts', component: PostsPage }
+    { path: '/posts', name: 'posts', component: PostsPage },
+    { path: '/posts/:id', name: 'post-detail', component: PostDetailPage }
   ],
 })    
 
